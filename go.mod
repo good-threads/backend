80a27ecbf1dbbfbd2782d5b6be27d901f3afaf8b
Thomas Marlow <43114877+thomasmarlow@users.noreply.github.com>
2023-12-17 21:30:58 -0300

    read env vars

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/good-threads/backend
 
 go 1.21.5
 
-require github.com/go-chi/chi/v5 v5.0.10
+require (
+	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-chi/chi/v5 v5.0.10
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
 )
 
+type Config struct {
+	Route string `env:"ROUTE,required"`
+}
+
 func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("unable to load .env file: %e", err)
+	}
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatalf("unable to parse ennvironment variables: %e", err)
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.Get(cfg.Route, func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome\n"))
 	})
+
+	log.Println("Listening...")
+
 	http.ListenAndServe(":3000", r)
 }
