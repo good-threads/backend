dc3ddcca4c0ce7c8b74ee8daef7f8051affc42e3
Thomas Marlow <43114877+thomasmarlow@users.noreply.github.com>
2023-12-24 13:25:23 -0300

    better test naming

--- internal/logic/user/user_test.go
+++ internal/logic/user/user_test.go
@@ -9,23 +9,23 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-func TestCreateFailsIfEmptyUsername(t *testing.T) {
+func TestCreateFailsOnEmptyUsername(t *testing.T) {
 	_, errorIsBadUsername := Setup(nil).Create("", "asd").(*e.BadUsername)
 	assert.True(t, errorIsBadUsername)
 }
 
-func TestCreateFailsIfEmptyPassword(t *testing.T) {
+func TestCreateFailsOnEmptyPassword(t *testing.T) {
 	_, errorIsBadPassword := Setup(nil).Create("asd", "").(*e.BadPassword)
 	assert.True(t, errorIsBadPassword)
 }
 
-func TestCreateFailsIfLongPassword(t *testing.T) {
+func TestCreateFailsOnLongPassword(t *testing.T) {
 	err := Setup(nil).
 		Create("asd", "12345678901234567890123456789012345678901234567890123456789012345678901234567890")
 	assert.NotNil(t, err)
 }
 
-func TestCreateOk(t *testing.T) {
+func TestCreateOK(t *testing.T) {
 	client := &user.MockClient{}
 	client.On("Persist", "asd", mock.Anything).Return(nil)
 	assert.Nil(t, Setup(client).Create("asd", "asd"))
